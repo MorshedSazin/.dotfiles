7563f4e88a13eb3e59a94fedb0003c82a5801b0f
MorshedSazin <morshedsazin@gmail.com>
2025-03-11 04:19:06 +0600

    fixed oh-my-bash

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.run.sh ~/.run.sh
 ln -s ~/.dotfiles/.run.sh.cp ~/.run.sh.cp
-ln -s ~/.dotfiles/.oh-my-bash ~/.oh-my-bash
 ln -s ~/.dotfiles/.nvim ~/.config/nvim
+git clone https://github.com/ohmybash/oh-my-bash.git ~/.oh-my-bash
