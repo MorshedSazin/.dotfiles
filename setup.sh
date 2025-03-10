93059193099fc8c9c1f1ffc3f0bc8409116a64f9
MorshedSazin <morshedsazin@gmail.com>
2025-03-11 03:35:42 +0600

    added .oh-my-bash setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.run.sh ~/.run.sh
+ln -s ~/.dotfiles/.run.sh.cp ~/.run.sh.cp
+ln -s ~/.dotfiles/.oh-my-bash ~/.oh-my-bash
+
