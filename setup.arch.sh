55f07edfbb18972d3b5a23e5b1f5ba438e9212ed
morshed sazin <morshedsazin@gmail.com>
2025-04-01 00:05:14 -0400

    added seperated file for devian and arch

--- setup.arch.sh
+++ setup.arch.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 #installing all the necessery packeges.
-source ./.pkg.debian.sh
-#source ./.pkg.arch.sh
+source ./.pkg.arch.sh
 
 #---------------------------------------------------
 #linking all the nessecery files for the envirment
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-#ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.run.sh ~/.run.sh
 ln -s ~/.dotfiles/.run.sh.cp ~/.run.sh.cp
@@ -19,5 +18,3 @@ ln -s ~/.dotfiles/.nvim ~/.config/nvim
 
 # for oh-my-bash
 #git clone https://github.com/ohmybash/oh-my-bash.git ~/.oh-my-bash
-# for oh-my-zsh 
-#cd ~/ && wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh && sh install.sh
---
+++ setup.debian.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#installing all the necessery packeges.
+source ./.pkg.debian.sh
+
+#---------------------------------------------------
+#linking all the nessecery files for the envirment
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.run.sh ~/.run.sh
+ln -s ~/.dotfiles/.run.sh.cp ~/.run.sh.cp
+ln -s ~/.dotfiles/.nvim ~/.config/nvim
+
+#---------------------------------------------------
+#cloning or donloading all the packages
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# for oh-my-zsh 
+cd ~/ && wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh && sh install.sh
